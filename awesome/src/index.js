a769602f3541d6ded61d7bc48985ab2de9a975cd
byang28 <byang28@illinois.edu>
2020-02-23 16:52:35 +0900

    added awesome

---
+++ awesome/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Item from './component/item' 
+
+const element = <h1>"this is awesome"</h1>
+ReactDOM.render(element,document.getElementById("root"))
+ReactDOM.render(<Item/>,document.getElementById('root2'))
