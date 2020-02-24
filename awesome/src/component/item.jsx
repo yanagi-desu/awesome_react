288208cd06ffea0b695114bebb82df2ec408a2fa
byang28 <byang28@illinois.edu>
2020-02-24 21:07:45 +0900

    added new styles

--- awesome/src/component/item.jsx
+++ awesome/src/component/item.jsx
@@ -5,7 +5,29 @@ import {render} from "react-dom"
 
 class Item extends Component{
     state={
-        count:1
+        count:0,
+        itemlist:[
+            {
+                id:0,
+                context:"primary",
+                modifier:"list-group-item list-group-item-primary"
+            },
+            {
+                id:1,
+                context:"secondary",
+                modifier:"list-group-item list-group-item-secondary"
+            },
+            {
+                id:2,
+                context:"success",
+                modifier:"list-group-item list-group-item-success"
+            },
+            {
+                id:3,
+                context:"danger",
+                modifier:"list-group-item list-group-item-danger"
+            }
+            ]
     }
 
     constructor(){
@@ -17,19 +39,50 @@ class Item extends Component{
         this.setState({count:this.state.count+=1});
     }
 
+    renderItems(){
+        if(this.state.itemlist.length === 0){
+            return(
+                <div className="alert alert-warning" role="alert">
+                    this is no item in the list 
+                </div>
+            )
+        }else{
+            return(
+                <ul className="list">
+                    {this.state.itemlist.map(item=>(
+                        <li key={item.id}
+                        className={item.modifier}>{item.context}</li>
+                    ))}
+                </ul>
+            )
+        }
+    }
     
     render(){
         return(
             <div>
+                <React.Fragment>
+                    <div className="card mb-2">
+                        <h5 className = {this.styleheader()}>{this.styleheader()}</h5>
+                    </div>
+                </React.Fragment>
                 <span style = {{border:"7px solid lightgrey"}}
                 className="badge badge-info m-1"
                 >{this.styleCount()}</span>
                 <button style = {{}}
                 className="badge badge-primary" onClick={this.handleclick} >Click!</button>
+                <React.Fragment>
+                    {this.renderItems()};
+                </React.Fragment>
             </div>
         );
     }
 
+    styleheader(){
+        let classes = "card-header h4 text-white bg-";
+        classes += this.state.count === 0?"warning":"primary";
+        return classes;
+    }
     styleCount(){
         return this.state.count === 0 ? "Nothing":this.state.count;
     }
